481d9c300fc2456e84fe543a5a6d582dfbfed20a
Mamun Hasan <mamunhasan@w3engineers.com>
2018-05-11 16:09:17 +0600

    remove test

--- real_time_object_detection_custom_pi.py
+++ real_time_object_detection_custom_pi.py
@@ -54,40 +54,73 @@ rawCapture = PiRGBArray(camera, size=(640, 480))
 time.sleep(0.1)
 
 # loop over the frames from the video stream
+# for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+#     image = frame.array
+#     (h, w) = image.shape[:2]
+#
+#     # blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
+#     blob = cv2.dnn.blobFromImage(cv2.resize(image, (224, 224)), 1, (224, 224), (104, 117, 123))
+#
+#     # pass the blob through the network and obtain the detections and predictions
+#     net.setInput(blob)
+#     detections = net.forward()
+#
+#     # sort the indexes of the probabilities in descending order (higher
+#     # probabilitiy first) and grab the top-5 predictions
+#     idxs = np.argsort(detections[0])[::-1][:5]
+#
+#     # loop over the top-5 predictions and display them
+#     for (i, idx) in enumerate(idxs):
+#         confidence = detections[0, idx]
+#
+#         # filter out weak detections by ensuring the `confidence` is
+#         # greater than the minimum confidence
+#         if confidence > args["confidence"]:
+#
+#             # draw the top prediction on the input image
+#             if i == 0:
+#                 text = "Label: {}, {:.2f}%".format(CLASSES[idx], confidence * 100)
+#                 cv2.putText(frame, text, (5, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+#
+#             # display the predicted label + associated probability to the console
+#             print("[INFO] {}. label: {}, probability: {:.5}".format(i + 1, CLASSES[idx], confidence))
+#
+#     # show the output frame
+#     cv2.imshow("Frame", frame)
+#     key = cv2.waitKey(1) & 0xFF
+#
+#     # if the `q` key was pressed, break from the loop
+#     if key == ord("q"):
+#         break
+
+detected_objects = []
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     image = frame.array
     (h, w) = image.shape[:2]
-
-    # blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
-    blob = cv2.dnn.blobFromImage(cv2.resize(image, (224, 224)), 1, (224, 224), (104, 117, 123))
-
-    # pass the blob through the network and obtain the detections and predictions
+    blob = cv2.dnn.blobFromImage(image, 0.007843, (300, 300), 127.5)
     net.setInput(blob)
     detections = net.forward()
-
-    # sort the indexes of the probabilities in descending order (higher
-    # probabilitiy first) and grab the top-5 predictions
-    idxs = np.argsort(detections[0])[::-1][:5]
-
-    # loop over the top-5 predictions and display them
-    for (i, idx) in enumerate(idxs):
-        confidence = detections[0, idx]
-
-        # filter out weak detections by ensuring the `confidence` is
-        # greater than the minimum confidence
+    for i in np.arange(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
         if confidence > args["confidence"]:
-
-            # draw the top prediction on the input image
-            if i == 0:
-                text = "Label: {}, {:.2f}%".format(CLASSES[idx], confidence * 100)
-                cv2.putText(frame, text, (5, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-
-            # display the predicted label + associated probability to the console
-            print("[INFO] {}. label: {}, probability: {:.5}".format(i + 1, CLASSES[idx], confidence))
-
-    # show the output frame
-    cv2.imshow("Frame", frame)
+            idx = int(detections[0, 0, i, 1])
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            label = "{}: {:.2f}%".format(CLASSES[idx],
+                                         confidence * 100)
+            detected_objects.append(label)
+            print(label)
+            cv2.rectangle(image, (startX, startY), (endX, endY),
+                          COLORS[idx], 2)
+            y = startY - 15 if startY - 15 > 15 else startY + 15
+            cv2.putText(image, label, (startX, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+    # show the frame
+    cv2.imshow("Frame", image)
     key = cv2.waitKey(1) & 0xFF
+
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
 
     # if the `q` key was pressed, break from the loop
     if key == ord("q"):
