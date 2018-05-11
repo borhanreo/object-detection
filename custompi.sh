05f7dd02b86e6fda692754031fcd05b9095cbc35
Mamun Hasan <mamunhasan@w3engineers.com>
2018-05-11 16:05:20 +0600

    remove test

---
+++ custompi.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python real_time_object_detection_custom_pi.py --prototxt dnn_deploy.prototxt --model dnn_deploy.caffemodel --labels custom_words.txt --source webcam
--- real_time_object_detection_custom_pi.py
+++ real_time_object_detection_custom_pi.py
@@ -7,8 +7,10 @@ import numpy as np
 import sys
 import argparse
 import imutils
-import time
 import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
 
 if sys.version_info[0] < 3:
     from urllib2 import urlopen
@@ -42,32 +44,22 @@ COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 print("[INFO] loading model...")
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
-# initialize the video stream, allow the cammera sensor to warmup,
-print("[INFO] starting video stream...")
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+#camera.resolution = (300, 300)
+camera.framerate = 10
+rawCapture = PiRGBArray(camera, size=(640, 480))
+#rawCapture = PiRGBArray(camera, size=(300, 300))
+time.sleep(0.1)
 
-if args["source"] == "webcam":
-    vs = cv2.VideoCapture(0)
-
-time.sleep(2.0)
-
-detected_objects = []
 # loop over the frames from the video stream
-while True:
-    # grab the frame from the threaded video stream and resize it
-    # to have a maximum width of 400 pixels
-    if args["source"] == "webcam":
-        ret, frame = vs.read()
-    else:
-        imgResp = urlopen(url)
-        imgNp = np.array(bytearray(imgResp.read()), dtype=np.uint8)
-        frame = cv2.imdecode(imgNp, -1)
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    image = frame.array
+    (h, w) = image.shape[:2]
 
-    frame = imutils.resize(frame, width=800)
-
-    # grab the frame dimensions and convert it to a blob
-    (h, w) = frame.shape[:2]
     # blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
-    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (224, 224)), 1, (224, 224), (104, 117, 123))
+    blob = cv2.dnn.blobFromImage(cv2.resize(image, (224, 224)), 1, (224, 224), (104, 117, 123))
 
     # pass the blob through the network and obtain the detections and predictions
     net.setInput(blob)
