c80d57388395bef3e5d472a228a909e3efa61158
Mamun Hasan <mamunhasan@w3engineers.com>
2018-05-11 16:21:59 +0600

    remove test

--- real_time_object_detection_custom_pi.py
+++ real_time_object_detection_custom_pi.py
@@ -48,14 +48,15 @@ camera = PiCamera()
 camera.resolution = (640, 480)
 # camera.resolution = (300, 300)
 camera.framerate = 10
-rawCapture = PiRGBArray(camera, size=(640, 480))
+rawCapture = PiRGBArray(camera, size=(224, 224))
 # rawCapture = PiRGBArray(camera, size=(300, 300))
 time.sleep(0.1)
 detected_objects = []
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     image = frame.array
     (h, w) = image.shape[:2]
-    blob = cv2.dnn.blobFromImage(image, 0.007843, (300, 300), 127.5)
+    # blob = cv2.dnn.blobFromImage(image, 0.007843, (224, 224), 127.5)
+    blob = cv2.dnn.blobFromImage(cv2.resize(image, (224, 224)), 1, (224, 224), (104, 117, 123))
     net.setInput(blob)
     detections = net.forward()
 
