a4ea1a6b1f1d89209331118d0d0523856d66b351
Mamun Hasan <mamunhasan@w3engineers.com>
2018-06-27 18:54:03 +0600

    distance measure

--- distance_to_camera.py
+++ distance_to_camera.py
@@ -1,61 +1,65 @@
-# import the necessary packages
-from imutils import paths
 import numpy as np
-import imutils
 import cv2
 
 
 def find_marker(image):
-    # convert the image to grayscale, blur it, and detect edges
+    # convert the image to grayscale and blur to detect edges
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (5, 5), 0)
     edged = cv2.Canny(gray, 35, 125)
 
-    # find the contours in the edged image and keep the largest one;
-    # we'll assume that this is our piece of paper in the image
-    cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+    # find the contours in the edged image
+
+    (cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     c = max(cnts, key=cv2.contourArea)
 
-    # compute the bounding box of the of the paper region and return it
     return cv2.minAreaRect(c)
 
+
 def distance_to_camera(knownWidth, focalLength, perWidth):
-	# compute and return the distance from the maker to the camera
-	return (knownWidth * focalLength) / perWidth
+    # compute and return the distance from the image to the camera
+    return (knownWidth * focalLength) / perWidth
 
 
-# initialize the known distance from the camera to the object, which
-# in this case is 24 inches
-KNOWN_DISTANCE = 24.0
+# initialize the known distance from the camera to the object
+KNOWN_DISTANCE = 20.0
 
 # initialize the known object width, which in this case, the piece of
-# paper is 12 inches wide
-KNOWN_WIDTH = 11.0
+# paper is 21 cm wide
+KNOWN_WIDTH = 21.0
 
-IMAGE_PATHS = []
-
-# load the furst image that contains an object that is KNOWN TO BE 2 feet
-# from our camera, then find the paper marker in the image, and initialize
-# the focal length
+# initialize the list of images that we'll be using
+# (the first image is the one we know the distance from camera.)
+IMAGE_PATHS = ["D1e.jpg", "D-1e.jpg", "D2e.jpg"]
+# load the image that contains an object that is KNOWN TO BE from our camera
 image = cv2.imread(IMAGE_PATHS[0])
 marker = find_marker(image)
+print(marker[1][0])
 focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
+print(focalLength)
 
 # loop over the images
-for imagePath in sorted(paths.list_images("images")):
-    # load the image, find the marker in the image, then compute the
-    # distance to the marker from the camera
+for imagePath in IMAGE_PATHS:
+    # compute the distance to the paper from the camera
     image = cv2.imread(imagePath)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     marker = find_marker(image)
-    inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
+    CM = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
 
-    # draw a bounding box around the image and display it
-    box = cv2.cv.BoxPoints(marker) if imutils.is_cv2() else cv2.boxPoints(marker)
-    box = np.int0(box)
-    cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
-    cv2.putText(image, "%.2fft" % (inches / 12),
-                (image.shape[1] - 200, image.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
-                2.0, (0, 255, 0), 3)
+    # draw a circle around the image and display it which is also circle
+    circles = cv2.HoughCircles(gray, cv2.cv.CV_HOUGH_GRADIENT, 1.2, 100)
+    # ensure at least some circles were found
+    if circles is not None:
+        # convert the (x, y) coordinates and radius of the circles to integers
+        circles = np.round(circles[0, :]).astype("int")
+
+        # loop over the (x, y) coordinates and radius of the circles
+        for (x, y, r) in circles:
+            cv2.circle(image, (x, y), r, (0, 0, 0), 5)
+
+    cv2.putText(image, "%.2fcm" % CM,
+                (image.shape[1] - 350, image.shape[0] - 15), cv2.FONT_HERSHEY_SIMPLEX,
+                2.0, (255, 0, 0), 3)
     cv2.imshow("image", image)
     cv2.waitKey(0)
+    cv2.destroyAllWindows()
