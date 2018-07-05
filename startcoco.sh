fa90bbb3da3336714597d2063b57fbd01d39befe
Mamun Hasan <mamunhasan@w3engineers.com>
2018-07-05 12:59:13 +0600

    object distance

---
+++ startcoco.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python3 real_time_object_detection_coco.py --prototxt prototxt/VGG_coco_SSD_300x300_deploy.prototxt --model models/VGG_coco_SSD_300x300_iter_400000.caffemodel --source webcam
