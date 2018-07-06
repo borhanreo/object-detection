642b348cd1b9a383251553f4756d384cfa686a73
Mamun Hasan <mamunhasan@w3engineers.com>
2018-07-06 12:59:05 +0600

    coco model object detection

--- camera.py
+++ camera.py
@@ -7,8 +7,12 @@ time.sleep(2.0)
 while True:
     ret, frame = vs.read()
 
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(gray, 35, 125)
+
     # show the output frame
-    cv2.imshow("Frame", frame)
+    cv2.imshow("Frame", gray)
     key = cv2.waitKey(1) & 0xFF
 
     # if the `q` key was pressed, break from the loop
